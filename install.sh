1c17f352b6e36c0cd040f1ef99e6f9dcdeba167e
dsrasheed <daniyaalrasheed@gmail.com>
2023-11-18 18:39:52 -0600

    Switched conda commands to pip3

--- install.sh
+++ install.sh
@@ -18,37 +18,31 @@
 git submodule update --init --recursive
 git submodule update --remote --merge # update to the latest version
 
-# install PyTorch
-conda deactivate
-conda env remove --name openpoints
-conda create -n openpoints -y python=3.7 numpy=1.20 numba
-conda activate openpoints
-
 # please always double check installation for pytorch and torch-scatter from the official documentation
-conda install -y pytorch=1.10.1 torchvision cudatoolkit=11.3 -c pytorch -c nvidia
-pip install torch-scatter -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
+pip3 install -y pytorch=1.10.1 torchvision cudatoolkit=11.3 -c pytorch -c nvidia
+pip3 install torch-scatter -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # install cpp extensions, the pointnet++ library
 cd openpoints/cpp/pointnet2_batch
-python setup.py install
+python3 setup.py install
 cd ../
 
 # grid_subsampling library. necessary only if interested in S3DIS_sphere
 cd subsampling
-python setup.py build_ext --inplace
+python3 setup.py build_ext --inplace
 cd ..
 
 
 # # point transformer library. Necessary only if interested in Point Transformer and Stratified Transformer
 cd pointops/
-python setup.py install
+python3 setup.py install
 cd ..
 
 # Blow are functions that optional. Necessary only if interested in reconstruction tasks such as completion
 cd chamfer_dist
-python setup.py install --user
+python3 setup.py install --user
 cd ../emd
-python setup.py install --user
+python3 setup.py install --user
 cd ../../../
